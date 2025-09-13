c97f5371f821f6d41ea367748740a01c5d142268
MrSuraj1 <surajyadavsy271@gmail.com>
2025-09-14 01:28:51 +0530

    Your commit message here

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
  origin: [
     "https://68c5c43758c598000849b4c7--navsatech.netlify.app/", // preview build
-    "https://navsatech.netlify.app",                          // final domain
+    "https://navsatech.netlify.app/",                          // final domain
      /\.netlify\.app$/ ,   
     "http://localhost:5173"                                   // local dev
   ],
