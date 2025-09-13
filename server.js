2d8b27581656053efe9277525d81d2cdc8e4fa01
MrSuraj1 <surajyadavsy271@gmail.com>
2025-09-14 01:34:36 +0530

    Your commit message here

--- server.js
+++ server.js
@@ -1,35 +1,45 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
 const shop = require("./routes/shop");
-const home = require('./routes/home');
-const orders = require('./routes/orderRoutes');
-const cors = require('cors');
+const home = require("./routes/home");
+const orders = require("./routes/orderRoutes");
+const cors = require("cors");
 
-const app = express();  // <-- initialize first
+const app = express(); // <-- initialize first
 const PORT = process.env.PORT || 5000;
 
 // CORS setup
-app.use(cors({
- origin: [
-    "https://68c5c43758c598000849b4c7--navsatech.netlify.app/", // preview build
-    "https://navsatech.netlify.app/",                          // final domain
-     /\.netlify\.app$/ ,   
-    "http://localhost:5173"                                   // local dev
-  ],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "https://68c5c43758c598000849b4c7--navsatech.netlify.app", // preview build
+      "https://navsatech.netlify.app", // final domain
+      /\.netlify\.app$/,                // allow ALL Netlify previews
+      "http://localhost:5173",          // local dev
+      "https://navsatech.com",
+      "https://www.navsatech.com",
+    ],
+    credentials: true,
+  })
+);
+
 
 app.use(express.json());
 
 // Routes
-app.use('/', home);
-app.use('/api/shop', shop);
-app.use('/api/orders', orders);
+app.use("/", home);
+app.use("/api/shop", shop);
+app.use("/api/orders", orders);
 
 // MongoDB connection
-mongoose.connect("mongodb+srv://surajyadavsy039:Suraj123@cluster0.yassbgc.mongodb.net/?retryWrites=true&w=majority")
+mongoose
+  .connect(
+    "mongodb+srv://surajyadavsy039:Suraj123@cluster0.yassbgc.mongodb.net/?retryWrites=true&w=majority"
+  )
   .then(() => {
     console.log("✅ MongoDB connected");
-    app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+    app.listen(PORT, () =>
+      console.log(`🚀 Server running on http://localhost:${PORT}`)
+    );
   })
   .catch((err) => console.error("❌ MongoDB connection error:", err));
